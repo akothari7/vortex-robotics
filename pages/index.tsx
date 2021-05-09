d32ac0629a5ed979771ca18967f0534c8587e4c5
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-05-08 22:33:18 -0700

    chore(App): refactor folder structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import Layout from "../components/Layout";
+import Layout from "../ui/general/Layout";
 
 const IndexPage = () => (
   <Layout title="Vortex Robotics | Home">
-    <h1>vortex robotics</h1>
+    <h1>Hello</h1>
   </Layout>
 );
 
