7ab4d05e30bd999d4150c446c16709dc6f230e67
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-05-12 16:53:41 -0700

    feat(SponsorPage): add animations

--- src/pages/OutreachPage.tsx
+++ src/pages/OutreachPage.tsx
@@ -3,6 +3,7 @@ import { Navbar } from "../ui/components/Navbar";
 import { LoremIpsum } from "../ui/utils/Paragraphs";
 import { Strong } from "../ui/utils/Strong";
 import blank from "../assets/white_background.png";
+import { Bounce, Fade, Roll } from "react-reveal";
 
 export const OutreachPage = () => {
   return (
@@ -10,38 +11,56 @@ export const OutreachPage = () => {
       <Navbar />
       <div className="outreach-content">
         <div className="header">
-          <h1 className="title">Outreach</h1>
-          <h2 className="outreach_p">
-            Introducing <Strong>STEM</Strong> and <Strong>FIRST</Strong> to our
-            communities.
-          </h2>
+          <Bounce>
+            <h1 className="title">Outreach</h1>
+          </Bounce>
+
+          <Fade>
+            <h2 className="outreach_p">
+              Introducing <Strong>STEM</Strong> and <Strong>FIRST</Strong> to
+              our communities.
+            </h2>
+          </Fade>
         </div>
 
         <div className="main-content">
-          <h1>
-            <Strong>2020-21</Strong> Season
-          </h1>
-
-          <div className="event">
-            <h2 className="date">December 2020</h2>
-            <LoremIpsum />
-            <img src={blank} width="500" />
-          </div>
-          <div className="event">
-            <h2 className="date">October 2020</h2>
-            <LoremIpsum />
-          </div>
-
-          <div className="space">
+          <Roll>
             <h1>
-              <Strong>2019-20</Strong> Season
+              <Strong>2020-21</Strong> Season
             </h1>
+          </Roll>
 
+          <Fade>
             <div className="event">
-              <h2 className="date">January 2020</h2>
+              <Bounce>
+                <h2 className="date">December 2020</h2>
+              </Bounce>
+
+              <LoremIpsum />
+              <img src={blank} width="500" />
+            </div>
+            <div className="event">
+              <Bounce>
+                <h2 className="date">October 2020</h2>
+              </Bounce>
               <LoremIpsum />
             </div>
-          </div>
+
+            <div className="space">
+              <Roll>
+                <h1>
+                  <Strong>2019-20</Strong> Season
+                </h1>
+              </Roll>
+
+              <div className="event">
+                <Bounce>
+                  <h2 className="date">January 2020</h2>
+                </Bounce>
+                <LoremIpsum />
+              </div>
+            </div>
+          </Fade>
         </div>
       </div>
     </div>
